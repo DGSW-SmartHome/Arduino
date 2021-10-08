048f94f883e405958d27350fa0a971f7672514c1
ahjeon0407 <63032730+ahjeon0407@users.noreply.github.com>
2021-10-08 23:54:03 +0900

    [UPDATE] ESP32_WIFI_test 주석 작성
    코드에 주석을 작성했습니다.

--- Test&Study/ESP32_WIFI_test/ESP32_WIFI_test_client/ESP32_WIFI_test_client.ino
+++ Test&Study/ESP32_WIFI_test/ESP32_WIFI_test_client/ESP32_WIFI_test_client.ino
@@ -1,12 +1,16 @@
+// FileName : ESP32_WIFI_test_client.ino
+// Describe : ESP32를 이용한 AP통신 테스트
+// History : 2021-10-07 JAH 
+// --------------------------------------------------------------------
 #include <WiFi.h>
 
-#define AP_ssid "Seven_Labs"
-#define AP_password "7777Labs"
-#define host "192.168.0.41"
-#define port 80
+#define AP_ssid "Seven_Labs"                  // WIFI NAME
+#define AP_password "7777Labs"                // WIFI PASSWORD
+#define host "192.168.0.41"                   // server IP Address
+#define port 80                               // server port
 
-IPAddress server(192,168,0,41);
-WiFiClient client;
+IPAddress server(192,168,0,41);               // server IP Address
+WiFiClient client;                            // 지정된 인터넷 IP 주소 및 포트에 연결할 수 있는 클라이언트 생성
 
 void setup() {
   Serial.begin(115200);
@@ -17,9 +21,9 @@ void setup() {
   Serial.print("Connecting to ");
   Serial.println(AP_ssid);
 
-  WiFi.begin(AP_ssid, AP_password);
+  WiFi.begin(AP_ssid, AP_password);            // WIFI 연결 시작
 
-  while (WiFi.status() != WL_CONNECTED) {
+  while (WiFi.status() != WL_CONNECTED) {      // WIFI가 연결이 되어 있지 않다면
       delay(500);
       Serial.print(".");
   }
@@ -27,16 +31,16 @@ void setup() {
   Serial.println("");
   Serial.println("WiFi connected");
 
-  if(client.connect(server, 80)) {
+  if(client.connect(server, 80)) {             // 클라이언트가 연결됐다면
     Serial.println("server connected");
   }
 
-  client.connect(server, port);
+  client.connect(server, port);                // 지정된 IP 주소와 포트에 연결
 }
 
 void loop() {
-  if(client.available()){
-    char c = client.read();
+  if(client.available()){                      // 서버가 클라이언트에 쓴 데이터가 있는가?
+    char c = client.read();                    // 서버에서 수신한 데이터 읽어오기
     Serial.println(c);
   }
   
--- Test&Study/ESP32_WIFI_test/ESP32_WIFI_test_server/ESP32_WIFI_test_server.ino
+++ Test&Study/ESP32_WIFI_test/ESP32_WIFI_test_server/ESP32_WIFI_test_server.ino
@@ -1,10 +1,14 @@
+// FileName : ESP32_WIFI_test_server.ino
+// Describe : ESP32를 이용한 AP통신 테스트
+// History : 2021-10-07 JAH 
+// --------------------------------------------------------------------
 #include <WiFi.h>
 
-#define AP_ssid "Seven_Labs"
-#define AP_password "7777Labs"
+#define AP_ssid "Seven_Labs"                  // WIFI NAME
+#define AP_password "7777Labs"                // WIFI PASSWORD
 int status = WL_IDLE_STATUS;
 
-WiFiServer server(80);
+WiFiServer server(80);                        // 지정된 포트에서 들어오는 연결을 수신 대기하는 서버 생성
 
 void setup() {
   Serial.begin(115200);
@@ -15,33 +19,33 @@ void setup() {
   Serial.print("Connecting to ");
   Serial.println(AP_ssid);
 
-  WiFi.begin(AP_ssid, AP_password);
+  WiFi.begin(AP_ssid, AP_password);            // WIFI 연결 시작
 
-  while (WiFi.status() != WL_CONNECTED) {
+  while (WiFi.status() != WL_CONNECTED) {      // WIFI가 연결이 되어 있지 않다면
       delay(500);
       Serial.print(".");
   }
 
-  server.begin();
+  server.begin();                               // 서버 시작
   Serial.println("");
   Serial.println("Server Start!");
   Serial.println("");
   Serial.println("WiFi connected");
   Serial.print("IP address: ");
-  Serial.println(WiFi.localIP());
+  Serial.println(WiFi.localIP());               // 와이파이 IP 주소 출력
 
 }
 
 void loop() {
-  WiFiClient client = server.available();
+  WiFiClient client = server.available();        // 서버에 연결되어 있는 클라이언트를 가져옴
   
   if(client){
 
-    if(client.connected()){
+    if(client.connected()){                       // 클라이언트가 연결되었는가?
       Serial.println("Connected to client");
-      client.write("Connected to server");
+      client.write("Connected to server");        // 클라이언트에 메세지 전달 - 수정예정
     }
   }
-//  client.stop();
+//  client.stop();                                // 클라이언트 닫기
   delay(1000);
 }
