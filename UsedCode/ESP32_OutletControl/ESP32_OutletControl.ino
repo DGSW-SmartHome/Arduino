604d54ff065f5e732197b8bf5da8d1ecb3bcca4a
KimJihun-1315 <65662529+KimJihun-1315@users.noreply.github.com>
2021-10-12 21:41:03 +0900

    Update ESP32_OutletControl.ino
    와이파이 통신 기능 추가

--- UsedCode/ESP32_OutletControl/ESP32_OutletControl.ino
+++ UsedCode/ESP32_OutletControl/ESP32_OutletControl.ino
@@ -1,13 +1,28 @@
-//----------------------------------------------------------------------------
+//------------------------------------------------------------------
 // Title    : ESP32_OutletControl
 // Function : Outlet control cords for use with ESP32 boards
 // History  : 20211007
-//----------------------------------------------------------------------------
+//------------------------------------------------------------------
 
+#include <WiFi.h>
+
+//시리얼 통신의 보드레이트설정
+#define BOADRATE 115200
+//와이파이 통신에 필요한 정보
+#define AP_SSID "Seven_Labs"
+#define AP_PSWD "7777Labs"
+#define HOST_IP "192.168.0.41"
+#define PORT 80
+//센서들의 핀설정
 #define OUTLET_PIN 13
 #define LEDR_PIN 10        //LED to indicate status
 #define LEDG_PIN 11
-#define BOADRATE 115200
+//시리얼(true)(int), 와이파이(false)(char) 통신 방식을 설정하는
+#define COM_METHOD false
+#define TYPE char
+
+IPAddress server(192,168,0,41);
+WiFiClient client;
 
 //flag = 1 : Concent ON
 //flag = 0 : Concent OFF
@@ -16,6 +31,23 @@ int flag = 0;
 void setup() {
   Serial.begin(BOADRATE);
   Serial.print("start");
+
+  // We start by connecting to a WiFi network
+  Serial.println();
+  Serial.print("Connecting to ");
+  Serial.println(AP_SSID);
+
+  WiFi.begin(AP_SSID, AP_PSWD);
+  while (WiFi.status() != WL_CONNECTED) {
+      delay(500);
+      Serial.print(".");}
+      
+  Serial.println("");
+  Serial.println("WiFi connected");
+  
+  if(client.connect(server, PORT)){Serial.println("server connected");}
+  
+  client.connect(server, PORT);
 
   pinMode(OUTLET_PIN, OUTPUT);
   pinMode(LEDR_PIN, OUTPUT);
@@ -30,13 +62,21 @@ void loop(){
 }
 
 int communication(){
-  int value;
+  TYPE value = 0;
 
-  if(Serial.available()){
-    value = Serial.read();
-    if(value == '1'){
-      if(flag == 0){return 1;}
-      if(flag == 1){return 0;}
+  //시리얼 통신코드
+  if(COM_METHOD == true){
+    if(Serial.available()){
+      value = Serial.read();  //int형
+      return value;
+    }
+  }
+
+  //WiFi 통신
+  if(COM_METHOD == false){
+    if(client.available()){
+      value = client.read();  //char형
+      Serial.println(value);
     }
   }
 }
