4d84e595058ad6f68306a4e756e128913a8ba4dc
ahjeon0407 <63032730+ahjeon0407@users.noreply.github.com>
2021-10-18 15:12:45 +0900

    [ADD] ESP32_MAIN code
    메인 보드에 사용할 코드입니다.

--- UsedCode/ESP32_main.ino/ESP32_main.ino.ino
+++ UsedCode/ESP32_main.ino/ESP32_main.ino.ino
@@ -16,21 +16,42 @@
 #define PORT 80                       // Server PORT
 
 WiFiServer server(PORT);              // 지정된 포트에서 들어오는 연결을 수신 대기하는 서버 생성
+WiFiClient client;
 
-
+String recvCmd = null;
+String recvStat = null;
+int light[6] = { 0,0,0,0,0,0 };
 
 void setup() {
   Serial.begin(BOADRATE);
 
   set();
-
 }
 
 void loop() {
-  // put your main code here, to run repeatedly:
+  client = server.available();
+  
+  if(client) {
+    Serial.println("Connected to Client");
+    while(client.connected()) {
+      sendCmd();
 
+      if(client.available()) {
+        recvStat();
+      }
+    }
+  }
+
+  if(Serial.available()){
+    recvCmd();
+  }
+
+  sendStat();
+
+  delay(100);
 }
 
+//WIFI Connect & Server Connect function
 void set() {
   Serial.println();
   Serial.print("Conneting to ");
@@ -52,4 +73,74 @@ void set() {
   Serial.println("Server Start!");
   Serial.print("IP address: ");
   Serial.println(WiFi.localIP());
+}
+
+// 아두이노에게 제어메세지 전송
+void sendCmd() {
+  client.write((char*)recvCmd.c_str(), recvCmd.length());
+  client.write('\r');
+}
+
+// 아두이노에서 상태 메세지 받기
+void recvStat() {
+  recvStat = client.readStringUntil('\r');
+  Serial.println(recvStat);
+
+  if(cmd.substring(0,2) == "L1") light[0] = cmd.substring(3,4).toInt();
+  else if(cmd.substring(0,2) == "L2") light[1] = cmd.substring(3,4).toInt();
+  else if(cmd.substring(0,2) == "L3") light[2] = cmd.substring(3,4).toInt();
+  else if(cmd.substring(0,2) == "L4") light[3] = cmd.substring(3,4).toInt();
+  else if(cmd.substring(0,2) == "L5") light[4] = cmd.substring(3,4).toInt();
+  else if(cmd.substring(0,2) == "L6") light[5] = cmd.substring(3,4).toInt();
+}
+
+// 라즈베리파이에서 제어메세지 받기
+void recvCmd() {
+  recvCmdMsg = Serial.readStringUntil('\r');
+  Serial.println(recvCmdMsg);
+}
+
+// 라즈베리파이에게 상태 메세지 전송
+void sendStat() {
+  // finedust 
+  int finedust = 0;
+  
+#ifdef DEBUG
+  Serial.print("finedust: ");
+  Serial.print(finedust);
+#else
+  Serial.write("F");
+  Serial.write((byte*)&finedust, 4);
+#endif
+
+
+  // light stat
+#ifdef DEBUG
+  Serial.print(" / light1: ");
+  Serial.print(light[0]);
+  Serial.print(" / light2: ");
+  Serial.print(light[1]);
+  Serial.print(" / light3: ");
+  Serial.print(light[2]);
+  Serial.print(" / light4: ");
+  Serial.print(light[3]);
+  Serial.print(" / light5: ");
+  Serial.print(light[4]);
+  Serial.print(" / light6: ");
+  Serial.println(light[5]);
+#else
+  Serial.write("L1");
+  Serial.write((byte*)&light[0], 1);
+  Serial.write("L2");
+  Serial.write((byte*)&light[1], 1);
+  Serial.write("L3");
+  Serial.write((byte*)&light[2], 1);
+  Serial.write("L4");
+  Serial.write((byte*)&light[3], 1);
+  Serial.write("L5");
+  Serial.write((byte*)&light[4], 1);
+  Serial.write("L6");
+  Serial.write((byte*)&light[5], 1);
+  Serial.write('\n');
+#endif
 }
