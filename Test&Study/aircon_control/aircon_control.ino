eccb522d89c310f0bc99aa83aca27dc5505bfaaa
ahjeon0407 <63032730+ahjeon0407@users.noreply.github.com>
2021-10-19 23:53:33 +0900

    [UPDATE] aircon_control code 수정
    ir 수신 code를 수정했습니다.

--- Test&Study/aircon_control/aircon_control.ino
+++ Test&Study/aircon_control/aircon_control.ino
@@ -9,19 +9,43 @@
 #define RECV_PIN 2
 
 IRrecv irrecv(RECV_PIN);
-decode_results results;
+//decode_results results;
 
 void setup() {
   Serial.begin(BOADRATE);
   irrecv.enableIRIn();
+
+  Serial.print(F("Ready to receive IR signals at pin "));
+  Serial.println(RECV_PIN);
 }
 
-void loop() {
-  if(irrecv.decode(&results)) {             // 만약 IR센서로 신호가 들어온다면 데이터 주소 저장
+void loop() { // ReceiveDump 예제를 참고했음.
+  if(irrecv.decode()) {             // 만약 IR센서로 신호가 들어온다면 
+     Serial.println("Receive!");
 
-    Serial.print(results.value, HEX);
-    Serial.println();
+    if (irrecv.decodedIRData.flags & IRDATA_FLAGS_WAS_OVERFLOW) {
+      Serial.println("IR code too long. Edit IRremoteInt.h and increase RAW_BUFFER_LENGTH");
+    } else {
+      Serial.println();                               
+      Serial.println();
+      irrecv.printIRResultShort(&Serial);
+      Serial.println();
+      Serial.println(F("Raw result in internal ticks (50 us) - with leading gap"));
+      irrecv.printIRResultRawFormatted(&Serial, false); // Output the results in RAW format
+      Serial.println(F("Raw result in microseconds - with leading gap"));
+      irrecv.printIRResultRawFormatted(&Serial, true);  // Output the results in RAW format
+      Serial.println();                               // blank line between entries
+      Serial.print(F("Result as internal ticks (50 us) array - compensated with MARK_EXCESS_MICROS="));
+      Serial.println(MARK_EXCESS_MICROS);
+      irrecv.compensateAndPrintIRResultAsCArray(&Serial, false); // Output the results as uint8_t source code array of ticks
+      Serial.print(F("Result as microseconds array - compensated with MARK_EXCESS_MICROS="));
+      Serial.println(MARK_EXCESS_MICROS);
+      irrecv.compensateAndPrintIRResultAsCArray(&Serial, true); // Output the results as uint16_t source code array of micros
+      irrecv.printIRResultAsCVariables(&Serial);  // Output address and data as source code variables
 
-    irrecv.resume();
+      irrecv.compensateAndPrintIRResultAsPronto(&Serial);
+    }
+    delay(1000);
   }
+    irrecv.resume();
 }
